ab6909ecb384fe903620744575d91908c07a613e
Philip Loa <boyet007@gmail.com>
2018-08-14 22:22:19 +0700

    laravel with vue

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+
+                color: #636b6f;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 100vh;
+                margin: 0;
+            }
+
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('register') }}">Register</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                        <div id="app">
+                    <example-component></example-component>
+                        </div>
+                </div>
+           </div>
+        </div>
+        <script src="{{ asset('js/app.js') }}"></script>
+    </body>
+</html>
